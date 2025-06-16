76cf6a9d6c2bc698a64fbd004fbac3c5f496a89d
Arnav-0606 <arnavneginsut@gmail.com>
2025-06-16 20:54:37 +0530

    Time: 2 ms (22.90%) | Memory: 15.5 MB (10.91%) - LeetSync

--- 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.cpp
+++ 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.cpp
@@ -1,37 +1,24 @@
 class Solution {
 public:
-bool checkCOl(vector<vector<int>>&mat,int m,int n,int row,int col){
-  int msf = mat[row][col];
-  for(int i = 0;i<n;i++){
-    if(mat[i][col]>msf){
-      msf = mat[i][col];
-    }
-  }
-  if(msf == mat[row][col]){
-    return true;
-  }
-  else{
-    return false;
-  }
-}
     vector<int> luckyNumbers(vector<vector<int>>& mat) {
-    int n = mat.size();
-    int m = mat[0].size();
-    vector<int>ans;
-    int curr_row =0;
-    while(curr_row<n){
-    int min_in_row=0;
-    for(int i=0;i<m;i++){
-        if(mat[curr_row][i]<mat[curr_row][min_in_row]){
-        min_in_row= i;
+        int rows = mat.size();
+        int cols = mat[0].size();
+        vector<int> rowMin(rows, INT_MAX);
+        vector<int> colMax(cols, INT_MIN);
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                rowMin[i] = min(rowMin[i], mat[i][j]);
+                colMax[j] = max(colMax[j], mat[i][j]);
+            }
+        }
+        vector<int> ans;
+        for (int i = 0; i<rows;++i) {
+            for (int j = 0;j<cols; ++j) {
+                if (mat[i][j] == rowMin[i] && mat[i][j] == colMax[j]) {
+                    ans.push_back(mat[i][j]);
+                }
+            }
+        }
+        return ans;
     }
-  }
-  if(checkCOl(mat,m,n,curr_row,min_in_row)){
-    ans.push_back(mat[curr_row][min_in_row]);
-    break;
-  }
-  curr_row++;
-}
-return ans;    
-}
 };
