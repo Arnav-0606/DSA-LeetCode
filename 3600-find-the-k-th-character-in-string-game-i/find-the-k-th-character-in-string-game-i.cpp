284995e1c4c8d0a16216aea7e5fb5f438ffd34e4
Arnav-0606 <arnavneginsut@gmail.com>
2025-07-03 21:44:51 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (50.32%) - LeetSync

--- 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.cpp
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.cpp
@@ -3,13 +3,12 @@ public:
     string transform(string str){
         string t = str;
         for(int i=0;i<t.size();i++){
-            if(t[i]!='z'){
-                t[i] = (char)t[i]+1;
-            }
-            else{
+            if(t[i]=='z'){
                 t[i] = 'a';
             }
-
+            else{
+                t[i] = (char)t[i]+1;
+            }
         }
         return str + t;
     }
