5ef0894aebc7efa27068f9e38a4b3ea94c2a735c
Arnav-0606 <arnavneginsut@gmail.com>
2025-07-19 22:32:07 +0530

    Time: 32 ms (64.29%) | Memory: 125.5 MB (7.14%) - LeetSync

--- 3936-split-array-by-prime-indices/split-array-by-prime-indices.cpp
+++ 3936-split-array-by-prime-indices/split-array-by-prime-indices.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     bool checkPrime(int n){
-        if (n <= 1) return false;
-        for (int i = 2; i * i <= n; i++) {
-            if (n % i == 0) return false;
+        if(n <= 1) return false;
+        for(int i =2; i*i <=n;i++){
+            if(n%i==0) return false;
         }
         return true;
     }
-
     long long splitArray(vector<int>& nums) {
-        long long A = 0, B = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            if (checkPrime(i)) {
-                A += nums[i];
-            } else {
-                B += nums[i];
+        long long A = 0;
+        long long B = 0;
+        for(int i =0;i<nums.size();i++){
+            if(checkPrime(i)){
+                A+=nums[i];
+            }
+            else{
+                B+=nums[i];
             }
         }
-        return abs(A - B);
+        return abs(A-B);
     }
 };
