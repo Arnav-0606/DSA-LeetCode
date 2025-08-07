3ee4bb9c4c7ee169e770186ba69f22e097dde7f3
Arnav-0606 <arnavneginsut@gmail.com>
2025-08-07 22:59:31 +0530

    Time: 2 ms (59.66%) | Memory: 146.9 MB (22.86%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
@@ -15,15 +15,7 @@ public:
         if(!root) return 0;
         int lmd = minDepth(root->left);
         int rmd = minDepth(root->right);
-        if(lmd==0 and rmd !=0){
-            return rmd+1;
-        }
-        if(rmd ==0 and lmd !=0){
-            return lmd+1;
-        }
-        if(lmd ==0 and rmd ==0){
-            return 1;
-        }
+        if(!lmd or !rmd) return lmd + rmd + 1;
         return min(lmd,rmd)+1;
     }
 };
