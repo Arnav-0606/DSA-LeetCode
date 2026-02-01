bcdf555fa85cd2f40c0242abab9bcd02c41cdef2
Arnav-0606 <arnavneginsut@gmail.com>
2026-02-01 14:45:18 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (8.93%) - LeetSync

--- 4194-count-monobit-integers/count-monobit-integers.cpp
+++ 4194-count-monobit-integers/count-monobit-integers.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     int countMonobit(int n) {
-        int ans =1;
-        for(int i=1;i<=n;i++){
-            if((i & i+1) == 0) ans++;
-        }
-        return ans;
+        return 1+log2(n+1);
     }
 };
