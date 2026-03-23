de42bb76b3045e8d98b6ceb67014190cf83348a6
Arnav-0606 <arnavneginsut@gmail.com>
2026-03-23 14:23:57 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (54.96%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
@@ -1,21 +1,12 @@
 class Solution {
 public:
     long long countCommas(long long n) {
-        if(n<1e3) return 0;
-        else if(n < 1e6){
-            return n - 1e3 +1;
+        long long t = 1e3;
+        long long ans = 0;
+        while(n>=t){
+            ans += (n-t+1);
+            t*= 1e3;
         }
-        else if(n<1e9){
-            return (n- 1e3 + 1) + (n-1e6+1);
-        }
-        else if(n<1e12){
-            return (n-1e3+1) + (n-1e6+1) + (n-1e9+1);
-        }
-        else if(n<1e15){
-            return (n-1e3+1) +(n-1e6+1) + (n-1e9+1) + (n-1e12+1);
-        }
-        else{
-            return (n-1e3+1) +(n-1e6+1) + (n-1e9+1) + (n-1e12+1)+ (n-1e15+1);
-        }        
+        return ans;
     }
 };
