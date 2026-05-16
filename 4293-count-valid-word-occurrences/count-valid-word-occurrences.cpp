d7c417f5c59c0d489e67f33f649456d1702bbe6d
Arnav-0606 <arnavneginsut@gmail.com>
2026-05-16 23:43:25 +0530

    Time: 116 ms (49.00%) | Memory: 239.4 MB (24.56%) - LeetSync

---
+++ 4293-count-valid-word-occurrences/count-valid-word-occurrences.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool ch(char c){
+        return (c>='a' and c<='z');
+    }
+    vector<int> countWordOccurrences(vector<string>& chunks, vector<string>& queries) {
+        string s = "";
+        for(string str:chunks){
+            s+= str;
+        }
+        string word = "";
+        unordered_map<string,int>mp;
+        int n = s.size();
+        for(int i=0;i<s.size();i++){
+            if((ch(s[i])) or (s[i]=='-' and i!=0 and i!=n-1 and ch(s[i-1]) and ch(s[i+1]))){
+                word += s[i];
+            }
+            else{
+                mp[word]++;
+                word = "";
+            }
+        }
+        mp[word]++;
+        vector<int>ans;
+        for(int i=0;i<queries.size();i++){
+            ans.push_back(mp[queries[i]]);
+        }
+        return ans;
+    }
+};
