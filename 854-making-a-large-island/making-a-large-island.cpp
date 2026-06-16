edc4308251e777b8bc259c37635e7a0c1df12e9c
Arnav-0606 <arnavneginsut@gmail.com>
2026-06-16 22:49:22 +0530

    Time: 268 ms (52.74%) | Memory: 103 MB (94.16%) - LeetSync

--- 854-making-a-large-island/making-a-large-island.cpp
+++ 854-making-a-large-island/making-a-large-island.cpp
@@ -2,11 +2,13 @@ class Solution {
 public:
     int dx[4] = {0,-1,0,1};
     int dy[4] = {-1,0,1,0};
+    int mxArea = 0;
     void dfs(auto & grid,int i,int j,int n,int k,auto & mp){
         if(i<0 or i>=n or j<0 or j>=n) return;
         if(grid[i][j]!=1) return;
         grid[i][j] = k;
         mp[k]++;
+        mxArea = max(mxArea,mp[k]);
         for(int u=0;u<4;u++){
             int a = i+ dx[u];
             int b = j + dy[u];
@@ -16,7 +18,6 @@ public:
     int largestIsland(vector<vector<int>>& grid) {
         int k =2;
         map<int,int>mp;
-        int mxArea = 0;
         int n = grid.size();
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
@@ -44,11 +45,6 @@ public:
                 }
             }
         }
-        for(auto [k,v]:mp){
-            mxArea = max(mxArea,v);
-        }
         return mxArea;
-
-        
     }
 };
