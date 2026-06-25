8c12557df783761167c79a8d71ae56c9ec909857
Arnav-0606 <arnavneginsut@gmail.com>
2026-06-25 21:46:03 +0530

    Time: 87 ms (41.42%) | Memory: 76.1 MB (21.37%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -1,7 +1,12 @@
 class Solution {
 public:
-     int f(const string &s1,const string &s2,int m,int n,vector<vector<int>>&dp){
-        for(int i=m-1;i>=0;i--){
+    int longestPalindromeSubseq(string s) {
+        string s1 = s;
+        string s2 = s;
+        reverse(s2.begin(),s2.end());
+        int n = s.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        for(int i=n-1;i>=0;i--){
         for(int j=n-1;j>=0;j--){
         if(s1[i] == s2[j]){
         dp[i][j] = 1 + dp[i+1][j+1];
@@ -12,18 +17,6 @@ public:
         }
         }
         return dp[0][0];
-    }
-    int longestCommonSubsequence(string s1, string s2) {
-        int m = s1.size();
-        int n = s2.size();
-        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
-        return f(s1,s2,m,n,dp);
-    }
-
-    int longestPalindromeSubseq(string s) {
-        string s1 = s;
-        string s2 = s;
-        reverse(s2.begin(),s2.end());
-        return longestCommonSubsequence(s1,s2);
+        
     }
 };
