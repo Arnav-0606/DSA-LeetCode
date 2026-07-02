9135b4d802f5eb7e6be7f40e887fd48b527aabbb
Arnav-0606 <arnavneginsut@gmail.com>
2026-07-03 00:23:56 +0530

    Time: 1 ms (76.09%) | Memory: 11 MB (41.08%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,41 +1,20 @@
 class Solution {
 public:
-    bool f(int ws,int k,string &s){
-        int cnt =0;
-        int mx = INT_MIN;
-        int mn = INT_MAX;
-        int n = s.size();
-        vector<int>mp(26,0);
-        for(int i=0;i<ws;i++){
-            mp[s[i]-'A']++;
-            mx = max(mx,mp[s[i]-'A']);
-        }
-        if(ws-mx<=k) return true;
-        for(int i=ws;i<n;i++){
-            mp[s[i]-'A']++;
-            mp[s[i-ws]-'A']--;
-            mx = INT_MIN;
-            for(int i=0;i<26;i++){
-                mx = max(mp[i],mx);
-            }
-            mn = min(ws-mx,mn);
-        }
-        return mn<=k;
-    }
     int characterReplacement(string s, int k) {
         int n = s.size();
-        int lb = 1;
-        int ub = n;
-        int ans =-1;
-        while(lb<=ub){
-            int mid = (ub-lb)/2 +lb;
-            if(f(mid,k,s)){
-                ans = mid;
-                lb = mid+1;
+        vector<int>mp(26,0);
+        int mx = 0;
+        int ans = 0;
+        int i=0,j=0;
+        while(j<n){
+            mp[s[j]-'A']++;
+            mx = max(mx,mp[s[j]-'A']);
+            while((j-i+1)-mx >k){
+                mp[s[i]-'A']--;
+                i++;
             }
-            else{
-                ub = mid-1;
-            }
+            ans = max(ans,j-i+1);
+            j++;
         }
         return ans;
     }
