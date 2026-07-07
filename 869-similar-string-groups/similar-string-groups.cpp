6ffab1118d846d3acf819f3d89e2058e2eb91bf3
Arnav-0606 <arnavneginsut@gmail.com>
2026-07-07 07:10:17 +0530

    Time: 210 ms (26.78%) | Memory: 111 MB (30.44%) - LeetSync

---
+++ 869-similar-string-groups/similar-string-groups.cpp
@@ -0,0 +1,71 @@
+class DisjointSet{
+public:
+	vector<int>par;
+	vector<int>rank;
+	DisjointSet(int n){
+		par.resize(n);
+		rank.resize(n);
+	}
+	
+	void createSet(int x){
+par[x] = x;
+rank[x] = 0;
+	}
+
+	int findSet(int x){
+	if(par[x]==x) return x;
+	return  par[x] = findSet(par[x]);
+	}
+
+
+
+	void unionSet(int x,int y){
+		int px = findSet(x);
+		int py = findSet(y);
+		if(rank[px]==rank[py]){
+			rank[px]++;
+}
+if(rank[px]>rank[py]){
+par[py] = px;
+}
+else{
+	par[px]=py;
+}		
+}
+};
+
+class Solution{
+public :  
+	bool checkSimilar(string s1,string s2){
+    int swaps = 0;
+    int stringsSize = s1.size();
+        for(int i=0;i<stringsSize;i++){
+        	if(s1[i]!=s2[i]){
+        		swaps++;
+        	}
+        }
+        return swaps<=2;
+        }
+	int numSimilarGroups(vector<string>&strs){
+		int n = strs.size();
+		DisjointSet ds(n);
+		for(int i=0;i<n;i++){
+			ds.createSet(i);
+		}
+		for(int i=0;i<n;i++){
+	        for(int j =i+1;j<n;j++){
+		        if(ds.findSet(i)!=ds.findSet(j)){
+			        if(checkSimilar(strs[i],strs[j])){
+				    ds.unionSet(i,j);
+                    }
+		        }
+            }
+        }
+    unordered_set<int>groups;
+    for(int i=0;i<n;i++){
+    	groups.insert(ds.findSet(i));
+    }
+    return groups.size();
+		
+    }
+};
